68999112f90ec67ba8398e05bd557bbd2f09616c
Diego <diegodsha@hotmail.com>
2021-04-27 13:26:55 -0500

    Remove useless code from models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,4 @@
 class Expense < ApplicationRecord
-  
-  after_update :create_group, if: :group_nil?
 
   belongs_to :user
 
@@ -11,15 +9,5 @@ class Expense < ApplicationRecord
   validates :amount, presence: true, numericality: { less_than: 10_000 }
 
   scope :most_recent, -> { where('created_at DESC') }
-
-def create_group
-      group_id = :group_id
-      GroupsExpense.create(group_id:group_id, expense_id:id) if group_id
-end
-
-def group_nil?
-  groups.size.zero?
-end
-
-
+  
 end
--- app/models/groups_expense.rb
+++ app/models/groups_expense.rb
@@ -1,13 +1,6 @@
 class GroupsExpense < ApplicationRecord
 
-  #after_create :set_default
-
   belongs_to :group
   belongs_to :expense
 
-  # def set_default
-  #   self.group_id = 0
-  #   puts 'hello'
-  # end
-  
 end
